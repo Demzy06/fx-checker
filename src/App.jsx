e56bf86bff364ed355468880d9c63b05a9444a58
olayode demola <demolaolayode6@gmail.com>
2026-07-28 17:10:40 +0100

    feat: add header, currencyPreview and rateChecker component to app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import CurrencyPreview from "./components/CurrencyPreview";
+import Header from "./components/Header";
+import RateChecker from "./components/RateChecker";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Header />
+      <CurrencyPreview />
+      <RateChecker />
+    </div>
+  );
 }
 
 export default App;
