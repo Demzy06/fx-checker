fbf246ff31b1b32fcf7059a7e5cb30f0072dae5b
olayode demola <demolaolayode6@gmail.com>
2026-07-28 21:48:55 +0100

    feat: add RateChecker component

---
+++ src/components/ReceiveCurrency.jsx
@@ -0,0 +1,30 @@
+import FlagPic from "../assets/images/flags/bg.webp";
+import ArrowDown from "../assets/images/icon-chevron-down.svg";
+
+function ReceiveCurrency() {
+  return (
+    <div className="bg-[#202022] p-3 rounded-2xl border border-[#3D3D3D]">
+      <h2 className="text-primary-gray uppercase text-[17px] font-medium">
+        Send
+      </h2>
+      <div className="mt-8 flex items-center">
+        <input
+          type="text"
+          placeholder="0.00"
+          className="w-full text-white font-bold text-[35px] placeholder:text-primary-gray outline-0"
+        />
+        <button className="w-38 ml-auto bg-[#333333] rounded-lg p-2.5 pl-2.5 pr-2.5 font-medium text-white flex items-center justify-between h-fit">
+          <span className="h-fit w-6 inline-block">
+            <img src={FlagPic} alt="" className="w-full rounded-full" />
+          </span>
+          <span className="inline-block">EUR</span>
+          <span className="inline-block ">
+            <img src={ArrowDown} alt="" />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ReceiveCurrency;
