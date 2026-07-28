f4c6db64afa14545946c9983d187716092e0b7aa
olayode demola <demolaolayode6@gmail.com>
2026-07-28 23:39:23 +0100

    feat: add CurrencyCount component and layout

---
+++ src/components/CurrencyCount.jsx
@@ -0,0 +1,15 @@
+function CurrencyCount() {
+  return (
+    <div className="flex items-center">
+      <ul className="flex justify-between text-primary-gray uppercase text-[11px] gap-x-6 list-d">
+        <li>
+          <span className="mr-3">33</span>Currencies
+        </li>
+        <li className="list-disc">EDD</li>
+        <li className="list-disc">ECB data</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CurrencyCount;
