2d2aea44e7be05b165060cf1f86be9833870c3b6
olayode demola <demolaolayode6@gmail.com>
2026-07-28 23:35:20 +0100

    feat: setup component layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,14 @@
-function Button() {
-  return <div></div>;
+function Button({ text, icon, onClick, styles }) {
+  return (
+    <button className={styles}>
+      {icon && (
+        <span>
+          <img src={icon} alt="icon" />
+        </span>
+      )}
+      {text}
+    </button>
+  );
 }
 
 export default Button;
