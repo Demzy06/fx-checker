ef278d2fe382dfb47ca9a869c1083689857a59a0
olayode demola <demolaolayode6@gmail.com>
2026-07-28 18:00:06 +0100

    feat: set up layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
+import CurrencyCount from "./CurrencyCount";
+import Logo from "./Logo";
+
 function Header() {
-  return <header></header>;
+  return (
+    <header className="pt-5 pl-3 pr-3 flex justify-between">
+      <Logo />
+      <CurrencyCount />
+    </header>
+  );
 }
 
 export default Header;
