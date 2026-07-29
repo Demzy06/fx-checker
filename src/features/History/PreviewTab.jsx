acdb8f36cb86dd7f08e79215d2f6df19bac60695
olayode demola <demolaolayode6@gmail.com>
2026-07-29 14:37:07 +0100

    feat: add PreviewTab component and set up layout

---
+++ src/features/History/PreviewTab.jsx
@@ -0,0 +1,10 @@
+function PreviewTab({ text, data }) {
+  return (
+    <div className="rounded-2xl p-3 pr-0 pl-4 bg-deep-gray border border-[#2E2E2E]">
+      <p className="text-primary-gray uppercase mb-3">Change</p>
+      <p className="text-white text-[17px]">0.2358</p>
+    </div>
+  );
+}
+
+export default PreviewTab;
