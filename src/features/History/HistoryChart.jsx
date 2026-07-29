1564f8cdd0cc3d586a6d291d8f1f4f067b4a128f
olayode demola <demolaolayode6@gmail.com>
2026-07-29 14:38:42 +0100

    feat: add HistoryChart component and set up layout

---
+++ src/features/History/HistoryChart.jsx
@@ -0,0 +1,16 @@
+function HistoryChart() {
+  return (
+    <div className="bg-deep-gray rounded-xl pl-5 pr-5 p-4 mb-10 border border-[#2E2E2E]">
+      <div className="flex justify-between items-center">
+        <h1 className="text-white text-[18px]">USD/EUR</h1>
+        <ul className="text-primary-gray flex justify-between text-[14px] h-fit gap-7">
+          <li>0.0673</li>
+          <li className="uppercase list-disc">May 14 16:00 CET</li>
+        </ul>
+      </div>
+      <div className="xoo chart-div h-70"></div>
+    </div>
+  );
+}
+
+export default HistoryChart;
