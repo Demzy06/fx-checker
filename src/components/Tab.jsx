0d8ae4057d58f8f4eab586b45698604b45f8c64d
olayode demola <demolaolayode6@gmail.com>
2026-07-29 14:42:04 +0100

    feat: setup component  code layout

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,5 +1,20 @@
+const tabs = ["History", "Compare", "Favorite", "Log"];
+
 function Tab() {
-  return <div></div>;
+  return (
+    <div className="mb-10 mt-10">
+      <div className="border border-light-gray p-2  rounded-lg bg-[#171719]">
+        <h2 className="text-white uppercase">History</h2>
+      </div>
+      <div className="hidden bg-secondary-gray rounded-lg mt-2 p-2">
+        <ul className=" flex flex-col gap-y-2">
+          {tabs.map((tab) => (
+            <li className="text-white p-1 pr-2 pl-2">{tab}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Tab;
