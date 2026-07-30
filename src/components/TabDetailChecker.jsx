86572b57ba5a845b14404dfb6d1b6a74593aa31b
olayode demola <demolaolayode6@gmail.com>
2026-07-30 15:01:17 +0100

    add: create TabDetailChecker component and set up layout

---
+++ src/components/TabDetailChecker.jsx
@@ -0,0 +1,11 @@
+import History from "../features/History/History";
+
+function TabDetailChecker() {
+  return (
+    <div>
+      <History />
+    </div>
+  );
+}
+
+export default TabDetailChecker;
