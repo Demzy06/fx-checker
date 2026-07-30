c42617e5436f446ae581d8eb104417298051130f
olayode demola <demolaolayode6@gmail.com>
2026-07-30 14:58:58 +0100

    feat: import components, wrap the all child components in the CurrencyListProvider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import CurrencyPreview from "./components/CurrencyPreview";
 import Header from "./components/Header";
-import RateChecker from "./components/RateChecker";
+import RateChecker from "./features/RateChecker/RateChecker";
+import Tab from "./components/Tab";
+import TabDetailChecker from "./components/TabDetailChecker";
+import { CurrencyListProvider } from "./context/CurrencyListContext";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <CurrencyPreview />
-      <RateChecker />
-    </div>
+    <CurrencyListProvider>
+      <div>
+        <Header />
+        <CurrencyPreview />
+        <main className="pl-4 pr-4">
+          <RateChecker />
+          <Tab />
+          <TabDetailChecker />
+        </main>
+      </div>
+    </CurrencyListProvider>
   );
 }
 
