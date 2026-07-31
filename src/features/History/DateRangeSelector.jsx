04ec97fa6ee32a8ee34659adc4a2c18888733ecc
olayode demola <demolaolayode6@gmail.com>
2026-07-31 23:37:23 +0100

    feat: import useHistory cutsom hook, implement an event handler to pass clicked date range to state

--- src/features/History/DateRangeSelector.jsx
+++ src/features/History/DateRangeSelector.jsx
@@ -1,11 +1,17 @@
+import { useHistory } from "../../context/HistoryContext";
+
 const dateRange = ["1D", "1W", "1M", "3M", "1Y", "5Y"];
 
 function DateRangeSelector() {
+  const { setDateSelected, dateRangeOption } = useHistory();
   return (
     <div className="">
       <ul className="bg-deep-gray flex justify-between w-full rounded-lg pl-2 pr-2">
-        {dateRange.map((date) => (
-          <li className="p-2.5 text-[14px] pr-3 pl-3 text-primary-gray">
+        {dateRange.map((date, i) => (
+          <li
+            onClick={() => setDateSelected(dateRangeOption[i])}
+            className="p-2.5 text-[14px] pr-3 pl-3 text-primary-gray"
+          >
             {date}
           </li>
         ))}
