26c63f25f6227d6a4b8fdb641b89c66d4018b17b
olayode demola <demolaolayode6@gmail.com>
2026-07-31 23:29:59 +0100

    feat: import fromCurrency state and setter , made input a controlled element

--- src/features/RateChecker/SendCurrencyInput.jsx
+++ src/features/RateChecker/SendCurrencyInput.jsx
@@ -2,6 +2,7 @@ import FlagPic from "../../assets/images/flags/ar.webp";
 import ArrowDown from "../../assets/icons/icon-chevron-down.svg";
 import SelectCurrency from "./SelectCurrency";
 import { useCurrencyList } from "../../context/CurrencyListContext";
+import { useState } from "react";
 
 function SendCurrencyInput() {
   const {
@@ -11,7 +12,11 @@ function SendCurrencyInput() {
     showCurrencyListReceive,
     setShowCurrencyListReceive,
     setFromCurrency,
+    fromCurrency,
+    fromCurrencyValue,
+    setFromCurrencyValue,
   } = useCurrencyList();
+
   return (
     <div className="bg-secondary-gray p-3 rounded-2xl border border-light-gray">
       <h2 className="text-primary-gray uppercase text-[17px] font-medium">
@@ -19,6 +24,8 @@ function SendCurrencyInput() {
       </h2>
       <div className="mt-8 flex items-center">
         <input
+          value={fromCurrencyValue}
+          onChange={(e) => setFromCurrencyValue(e.target.value)}
           type="text"
           placeholder="0.00"
           className="w-full text-white font-bold text-[35px] placeholder:text-primary-gray outline-0"
@@ -33,9 +40,9 @@ function SendCurrencyInput() {
           className="w-31 ml-auto bg-[#2E2E2E] rounded-lg p-2.5 pl-2.5 pr-2.5 font-medium text-white flex items-center justify-between h-fit border border-light-gray"
         >
           <span className="h-fit w-5 inline-block">
-            <img src={FlagPic} alt="" className="w-full rounded-full" />
+            {/* <img src={FlagPic} alt="" className="w-full rounded-full" /> */}
           </span>
-          <span className="inline-block text-[14px]">EUR</span>
+          <span className="inline-block text-[14px]">{fromCurrency}</span>
           <span className="inline-block ">
             <img src={ArrowDown} alt="" />
           </span>
