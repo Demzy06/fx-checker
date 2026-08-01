7708a771d062635ffb3acf3776f9e0fd65916f9b
olayode demola <demolaolayode6@gmail.com>
2026-08-01 01:08:58 +0100

    feat: wrap compoents into the HistoryContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import RateChecker from "./features/RateChecker/RateChecker";
 import Tab from "./components/Tab";
 import TabDetailChecker from "./components/TabDetailChecker";
 import { CurrencyListProvider } from "./context/CurrencyListContext";
+import { HistoryContextProvider } from "./context/HistoryContext";
 
 function App() {
   return (
     <CurrencyListProvider>
-      <div>
-        <Header />
-        <CurrencyPreview />
-        <main className="pl-4 pr-4">
-          <RateChecker />
-          <Tab />
-          <TabDetailChecker />
-        </main>
-      </div>
+      <HistoryContextProvider>
+        <div>
+          <Header />
+          <CurrencyPreview />
+          <main className="pl-4 pr-4">
+            <RateChecker />
+            <Tab />
+            <TabDetailChecker />
+          </main>
+        </div>
+      </HistoryContextProvider>
     </CurrencyListProvider>
   );
 }
