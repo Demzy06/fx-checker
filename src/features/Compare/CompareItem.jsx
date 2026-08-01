782868c789a4b66b462150f63f81b2883cc122df
olayode demola <demolaolayode6@gmail.com>
2026-08-02 00:53:05 +0100

    feat: create CompareItems component and set up layout

---
+++ src/features/Compare/CompareItem.jsx
@@ -0,0 +1,30 @@
+import flag from "../../assets/images/flags/au.webp";
+import StarIcon from "../../assets/icons/icon-star.svg";
+
+export default function CompareItem({ rate }) {
+  return (
+    <div className="p-2.5 pl-3.5 pr-3.5 flex justify-between border border-light-gray rounded-lg bg-secondary-gray mb-3">
+      <div className="flex items-center">
+        <img
+          src={flag}
+          alt=""
+          srcset=""
+          className="mr-3 w-6 h-fit rounded-full"
+        />
+        <span>
+          <p className="uppercase text-white text-[14px]">{rate[0]}</p>
+          <p className="text-primary-gray text-[12px]">{rate[0]}</p>
+        </span>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-3 text-end">
+          <p className="text-white text-[14px]">0.69</p>
+          <p className="text-primary-gray text-[12px]">@ {rate[1]}</p>
+        </span>
+        <button className="border border-light-gray rounded-lg h-fit p-2 pl-2 pr-2 ">
+          <img src={StarIcon} alt="" srcset="" className="" />
+        </button>
+      </div>
+    </div>
+  );
+}
