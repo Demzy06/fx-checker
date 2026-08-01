271ba4759d75e0585c771fae9f99f896bd310f33
olayode demola <demolaolayode6@gmail.com>
2026-08-01 16:59:28 +0100

    feat: implement logic to switch between tabs

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+import { useTabContext } from "../context/TabContext";
+
 const tabs = ["History", "Compare", "Favorite", "Log"];
 
 function Tab() {
+  const { setSelectedTab, selectedTab } = useTabContext();
+  const [tabIsOpen, setTabIsOpen] = useState(false);
   return (
     <div className="mb-10 mt-10">
-      <div className="border border-light-gray p-2  rounded-lg bg-[#171719]">
-        <h2 className="text-white uppercase">History</h2>
+      <div
+        className="border border-light-gray p-2  rounded-lg bg-deep-gray"
+        onClick={() => setTabIsOpen((isOpen) => !isOpen)}
+      >
+        <h2 className="text-white uppercase">{selectedTab}</h2>
       </div>
-      <div className="hidden bg-secondary-gray rounded-lg mt-2 p-2">
+      <div
+        className={`${tabIsOpen ? "" : "hidden"} bg-secondary-gray rounded-lg mt-2 p-2 absolute w-[92%]`}
+      >
         <ul className=" flex flex-col gap-y-2">
           {tabs.map((tab) => (
-            <li className="text-white p-1 pr-2 pl-2">{tab}</li>
+            <li
+              onClick={() => {
+                setSelectedTab(tab);
+                setTabIsOpen(false);
+              }}
+              className="text-white p-1 pr-2 pl-2 uppercase"
+            >
+              {tab}
+            </li>
           ))}
         </ul>
       </div>
