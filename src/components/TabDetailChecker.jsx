79be6ccb2a9d05f05e8450eb8e1208717514d1f5
olayode demola <demolaolayode6@gmail.com>
2026-08-02 00:53:49 +0100

    feat: import Compare component

--- src/components/TabDetailChecker.jsx
+++ src/components/TabDetailChecker.jsx
@@ -1,9 +1,15 @@
 import { useTabContext } from "../context/TabContext";
+import Compare from "../features/Compare/Compare";
 import History from "../features/History/History";
 
 function TabDetailChecker() {
   const { selectedTab } = useTabContext();
-  return <div>{selectedTab === "History" && <History />}</div>;
+  return (
+    <div>
+      {selectedTab === "History" && <History />}
+      {selectedTab === "Compare" && <Compare />}
+    </div>
+  );
 }
 
 export default TabDetailChecker;
