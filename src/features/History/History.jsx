f58b210f55eb2a960a2799d66aa350b5dd5db62c
olayode demola <demolaolayode6@gmail.com>
2026-08-01 11:04:01 +0100

    feat: calculate the preview data

--- src/features/History/History.jsx
+++ src/features/History/History.jsx
@@ -1,15 +1,21 @@
+import { useHistory } from "../../context/HistoryContext";
 import DateRangeSelector from "./DateRangeSelector";
 import HistoryChart from "./HistoryChart";
 import PreviewTab from "./PreviewTab";
 
 function History() {
+  const { currencyPairHistory } = useHistory();
+  const startRate = currencyPairHistory[0];
+  const endRate = currencyPairHistory[currencyPairHistory.length - 1];
+  const open = startRate?.rate;
+  const change = +startRate?.rate - +endRate?.rate;
   return (
     <section>
       <div className="grid grid-cols-2 gap-3">
-        <PreviewTab />
-        <PreviewTab />
-        <PreviewTab />
-        <PreviewTab />
+        <PreviewTab text="Open" data={startRate?.rate} />
+        <PreviewTab text="Last" data={endRate?.rate} />
+        <PreviewTab text="Change" data={change.toFixed(4)} />
+        <PreviewTab text="% Change" data={change.toFixed(4)} />
       </div>
       <div className="mt-6 mb-6 w-fit">
         <DateRangeSelector />
