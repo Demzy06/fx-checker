18d19bb850051339db6c5d769500312690b39239
olayode demola <demolaolayode6@gmail.com>
2026-08-01 11:03:18 +0100

    refactor: change static content to a dynamic content

--- src/features/History/PreviewTab.jsx
+++ src/features/History/PreviewTab.jsx
@@ -1,8 +1,8 @@
 function PreviewTab({ text, data }) {
   return (
     <div className="rounded-2xl p-3 pr-0 pl-4 bg-deep-gray border border-[#2E2E2E]">
-      <p className="text-primary-gray uppercase mb-3">Change</p>
-      <p className="text-white text-[17px]">0.2358</p>
+      <p className="text-primary-gray uppercase mb-3">{text}</p>
+      <p className="text-white text-[17px]">{data}</p>
     </div>
   );
 }
