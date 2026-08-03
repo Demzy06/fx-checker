cd176c818190dcace418351fc1933bde75d90faa
olayode demola <demolaolayode6@gmail.com>
2026-08-03 14:37:40 +0100

    fix: add a callback ftn to the event handler

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ text, icon, onClick, styles }) {
   return (
-    <button className={`${styles} flex items-center`} onClick={onClick}>
+    <button className={`${styles} flex items-center`} onClick={() => onClick()}>
       {icon && (
         <span className="">
           <img src={icon} alt="icon" className="w-5 mr-2" />
