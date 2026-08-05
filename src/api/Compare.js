bdf1c594fd1c8b0b5fc06778c44f77c2cf083545
olayode demola <demolaolayode6@gmail.com>
2026-08-05 22:47:49 +0100

    feat: implement error handling

--- src/api/Compare.js
+++ src/api/Compare.js
@@ -1,17 +1,22 @@
 // if (latestCurrenciesRate !== "") return;
 export async function getLatestRates(base, setterFtn, setisLoading, setError) {
+  setError(false)
   try {
     setisLoading(true);
     const res = await fetch(
       ` https://api.frankfurter.dev/v1/latest?base=${base} `,
     );
-    if (!res.ok) throw new Error("Something went wrong, try again!");
+
+    console.log(res)
+    if (!res.ok) {
+      throw new Error("Something went wrong, try again!");
+    }
 
     const data = await res.json();
     setterFtn(data);
-    setisLoading(false);
-
+    setisLoading(false)
   } catch (error) {
+    setisLoading(false);
     setError(true)
     console.log(error.message);
   }
