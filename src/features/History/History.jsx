06f62d699f5a3b1bae1c5f9d67f9458e1deda805
olayode demola <demolaolayode6@gmail.com>
2026-08-05 15:25:54 +0100

    feat: add logic to calculate percentageRate difference , and also pass className prop to PreviewTab

--- src/features/History/History.jsx
+++ src/features/History/History.jsx
@@ -2,6 +2,8 @@ import { useHistory } from "../../context/HistoryContext";
 import DateRangeSelector from "./DateRangeSelector";
 import HistoryChart from "./HistoryChart";
 import PreviewTab from "./PreviewTab";
+import ChevronFilledGreen from "../../assets/icons/chevron-up-filled-green.svg";
+import ChevronFilledRed from "../../assets/icons/chevron-down-filled-red.svg";
 
 function History() {
   const { currencyPairHistory } = useHistory();
@@ -9,13 +11,33 @@ function History() {
   const endRate = currencyPairHistory[currencyPairHistory.length - 1];
   const open = startRate?.rate;
   const change = +startRate?.rate - +endRate?.rate;
+  const percentageRate =
+    ((startRate?.rate - endRate?.rate) / endRate?.rate) * 100;
+
   return (
     <section>
       <div className="grid grid-cols-2 gap-3">
-        <PreviewTab text="Open" data={open} />
-        <PreviewTab text="Last" data={endRate?.rate} />
-        <PreviewTab text="Change" data={change.toFixed(4)} />
-        <PreviewTab text="% Change" data={change.toFixed(4)} />
+        <PreviewTab
+          text="Open"
+          data={open}
+          className="text-white text-[17px]"
+        />
+        <PreviewTab
+          text="Last"
+          data={endRate?.rate}
+          className="text-white text-[17px]"
+        />
+        <PreviewTab
+          text="Change"
+          data={change.toFixed(4)}
+          className={`${percentageRate < 0 ? "text-primary-red" : "text-[#42eb05]"}  text-[17px]`}
+        />
+        <PreviewTab
+          text="% Change"
+          data={`${percentageRate.toFixed(2) > 0 ? "+" : ""}${percentageRate.toFixed(2)}%`}
+          className={`${percentageRate < 0 ? "text-primary-red" : "text-[#42eb05]"}  text-[17px]`}
+          icon={percentageRate < 0 ? ChevronFilledRed : ChevronFilledGreen}
+        />
       </div>
       <div className="mt-6 mb-6 w-fit">
         <DateRangeSelector />
