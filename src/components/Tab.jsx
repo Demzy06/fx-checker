97227c4797ea6c5792fcdfbd31581400f4b7d2d7
olayode demola <demolaolayode6@gmail.com>
2026-08-05 17:09:09 +0100

    style: adjust layout style

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useTabContext } from "../context/TabContext";
+import ChevronDown from "../assets/icons/icon-chevron-down.svg";
 
 const tabs = ["History", "Compare", "Favorites", "Logs"];
 
@@ -9,10 +10,11 @@ function Tab() {
   return (
     <div className="mb-5 mt-10">
       <div
-        className="border border-light-gray p-2  rounded-lg bg-deep-gray"
+        className="border border-light-gray p-2 pr-3 pl-3 rounded-lg bg-deep-gray flex justify-between items-center"
         onClick={() => setTabIsOpen((isOpen) => !isOpen)}
       >
         <h2 className="text-white uppercase">{selectedTab}</h2>
+        <img src={ChevronDown} alt="chevron-down" className="w-4" />
       </div>
       <div
         className={`${tabIsOpen ? "" : "hidden"} bg-secondary-gray rounded-lg mt-2 p-2 absolute w-[92%]`}
