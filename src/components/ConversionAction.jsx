f9d33f8a1706404971f2acd582d3233a896bb4ab
olayode demola <demolaolayode6@gmail.com>
2026-08-05 15:39:54 +0100

    feat: add StarIconFilled icon + implement logic to know if currenncy pair is in Favorites

--- src/components/ConversionAction.jsx
+++ src/components/ConversionAction.jsx
@@ -1,26 +1,34 @@
 import Button from "./Button";
 import StarIcon from "../assets/icons/icon-star.svg";
+import StarIconFilled from "../assets/icons/icon-star-filledblack.svg";
 import { useCurrencyList } from "../context/CurrencyListContext";
 
 function ConversionAction() {
-  const { baseCurrency, quoteCurrency, rateObj, addToFavorites, addToLogs } =
-    useCurrencyList();
+  const {
+    baseCurrency,
+    quoteCurrency,
+    rateObj,
+    addToFavorites,
+    addToLogs,
+    isCurrPairInFav,
+    baseCurrencyValue,
+  } = useCurrencyList();
 
   return (
     <div className="border-dashed border-light-gray pt-6 ">
       <p className="text-center text-white">
         1 {baseCurrency} = {rateObj.rate} {quoteCurrency}
       </p>
-      <span className="flex justify-between w-[78%] m-auto mt-3">
+      <span className="flex justify-between w-[80%] m-auto mt-3">
         <Button
-          text="Favorite"
-          styles="uppercase p-1 pr-3 pl-3 bg-primary-yellow text-[14px] rounded-lg "
-          icon={StarIcon}
+          text={isCurrPairInFav ? "Favorited" : "Favorite"}
+          styles={`uppercase p-1 pr-3 pl-3 ${isCurrPairInFav ? "bg-primary-yellow text-black" : "bg-deep-gray text-white border border-light-gray"}  text-[14px] rounded-lg`}
+          icon={isCurrPairInFav ? StarIconFilled : StarIcon}
           onClick={addToFavorites}
         />
         <Button
           text="Log Conversion"
-          styles="uppercase p-1 pl-2 pr-2 border border-primary-yellow rounded-lg text-white"
+          styles={`${baseCurrencyValue <= 0 ? "opacity-100" : "opacity-100"} uppercase p-1 pl-2 pr-2 border border-primary-yellow rounded-lg text-white`}
           onClick={addToLogs}
         />
       </span>
