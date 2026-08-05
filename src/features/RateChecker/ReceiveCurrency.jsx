49f0d70562474bde648aa046b15d908f1bba1021
olayode demola <demolaolayode6@gmail.com>
2026-08-05 15:07:28 +0100

    feat: add flag image to selectCurrency button + made style changes

--- src/features/RateChecker/ReceiveCurrency.jsx
+++ src/features/RateChecker/ReceiveCurrency.jsx
@@ -1,12 +1,14 @@
-import FlagPic from "../../assets/images/flags/ar.webp";
 import ArrowDown from "../../assets/icons/icon-chevron-down.svg";
 import SelectCurrency from "./SelectCurrency";
 import { useCurrencyList } from "../../context/CurrencyListContext";
-// import { useState } from "react";
-
-// const BASE = "https://api.frankfurter.dev/v2/currencies";
+import { useState } from "react";
+import { currencyToCountry } from "../../data/currencies";
+import ReactCountryFlag from "react-country-flag";
 
 function ReceiveCurrency() {
+  // Created this state to store seleted currency, so flag can be displayed in the btn
+  const [clickedCurr, setClickedCurr] = useState("USD");
+
   const {
     setCurrencyDisplay,
     showCurrencyListReceive,
@@ -23,22 +25,29 @@ function ReceiveCurrency() {
         Receive
       </h2>
       <div className="mt-8 flex items-center">
-        {/* <span className="font-bold text-[35px] text-primary-yellow">
-          1,0000
-        </span> */}
-        <output className="w-[80%] text-primary-yellow font-bold text-[35px]">
+        {/* Show conversion */}
+        <output className="w-full text-primary-yellow font-bold text-[35px]">
           {convertedRate.toFixed(2)}
         </output>
         <button
           onClick={() => {
-            // setCurrencyDisplay((displayed) => !displayed);
+            setCurrencyDisplay((displayed) => !displayed);
             setShowCurrencyListReceive((show) => !show);
             if (showCurrencyListSend === true) setShowCurrencyListSend(false);
           }}
-          className="w-38 ml-auto bg-[#333333] rounded-lg p-2.5 pl-2.5 pr-2.5 font-medium text-white flex items-center justify-between h-fit"
+          className="w-31 ml-auto bg-lightbg-gray rounded-lg p-2.5 pl-2.5 pr-2.5 font-medium text-white flex items-center justify-between h-fit border border-light-gray"
         >
           <span className="h-fit w-6 inline-block">
-            {/* <img src={FlagPic} alt="" className="w-full rounded-full" /> */}
+            <ReactCountryFlag
+              countryCode={currencyToCountry[clickedCurr]}
+              svg
+              style={{
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+            />
           </span>
           <span className="inline-block">{quoteCurrency}</span>
           <span className="inline-block ">
@@ -49,6 +58,7 @@ function ReceiveCurrency() {
           selectCurrency={setQuoteCurrency}
           show={setShowCurrencyListReceive}
           styles={`${showCurrencyListReceive ? "" : "hidden"} -bottom-29 `}
+          setClickedCurr={setClickedCurr}
         />
       </div>
     </div>
