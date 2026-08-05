038e6627871009bef640cedb03d149fc1bda7a7b
olayode demola <demolaolayode6@gmail.com>
2026-08-05 20:21:25 +0100

    feat: create component and basic layout

---
+++ src/components/LoadingMessage.jsx
@@ -0,0 +1,5 @@
+function LoadingMessage({ message }) {
+  return <div>{message}</div>;
+}
+
+export default LoadingMessage;
---
+++ src/components/Message.jsx
@@ -0,0 +1,9 @@
+function Message({ message }) {
+  return (
+    <div className="pt-20 pb-20">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default Message;
