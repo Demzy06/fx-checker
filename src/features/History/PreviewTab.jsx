b90df3dd6185dc29287105b3f72883ec8f669bbc
olayode demola <demolaolayode6@gmail.com>
2026-08-05 15:20:48 +0100

    feat: receive className and icon prop

--- src/features/History/PreviewTab.jsx
+++ src/features/History/PreviewTab.jsx
@@ -1,8 +1,11 @@
-function PreviewTab({ text, data }) {
+function PreviewTab({ text, data, className, icon }) {
   return (
     <div className="rounded-2xl p-3 pr-0 pl-4 bg-deep-gray border border-[#2E2E2E]">
       <p className="text-primary-gray uppercase mb-3">{text}</p>
-      <p className="text-white text-[17px]">{data}</p>
+      <span className="flex items-center">
+        {icon && <img src={icon} className="mr-1" />}
+        <p className={className}>{data}</p>
+      </span>
     </div>
   );
 }
