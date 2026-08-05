17563e92a96d074ebdcea0912e9352dae0a3c60f
olayode demola <demolaolayode6@gmail.com>
2026-08-05 15:30:33 +0100

    feat: implement logic to know which date was selected + use ternary operator to add class

--- src/features/History/DateRangeSelector.jsx
+++ src/features/History/DateRangeSelector.jsx
@@ -3,14 +3,14 @@ import { useHistory } from "../../context/HistoryContext";
 const dateRange = ["1D", "1W", "1M", "3M", "1Y", "5Y"];
 
 function DateRangeSelector() {
-  const { setDateSelected, dateRangeOption } = useHistory();
+  const { setDateSelected, dateRangeOption, dateSelected } = useHistory();
   return (
     <div className="">
-      <ul className="bg-deep-gray flex justify-between w-full rounded-lg pl-2 pr-2">
+      <ul className="bg-deep-gray flex justify-between w-f rounded-lg pl-2 pr-2 ">
         {dateRange.map((date, i) => (
           <li
             onClick={() => setDateSelected(dateRangeOption[i])}
-            className="p-2.5 text-[14px] pr-3 pl-3 text-primary-gray"
+            className={`${date == dateSelected.range ? "bg-lightbg-gray" : "bg-tranparent"} p-2 text-[12px] pr-3 pl-3 text-primary-gray rounded-lg `}
           >
             {date}
           </li>
