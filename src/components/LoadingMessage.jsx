d16a2603a5491799afbe7536cb3ae078eca00de0
olayode demola <demolaolayode6@gmail.com>
2026-08-05 22:50:14 +0100

    feat:  add layout and styling

--- src/components/LoadingMessage.jsx
+++ src/components/LoadingMessage.jsx
@@ -1,5 +1,9 @@
 function LoadingMessage({ message }) {
-  return <div>{message}</div>;
+  return (
+    <div className="pt-20 pb-20 p-5  bg-deep-gray border border-light-gray mb-6 rounded-xl flex justify-center">
+      <p className="text-primary-gray text-[15px]">{message}</p>
+    </div>
+  );
 }
 
 export default LoadingMessage;
