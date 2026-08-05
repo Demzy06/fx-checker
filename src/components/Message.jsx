97d1907daba50cdf936d6bb6e3d7bb8e3e040970
olayode demola <demolaolayode6@gmail.com>
2026-08-05 22:49:41 +0100

    feat: setup layout + styling

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,10 @@
-function Message({ message }) {
+function Message({ message, messageTwo, baseCurrency, quoteCurrency }) {
   return (
-    <div className="pt-20 pb-20">
-      <h1>{message}</h1>
+    <div className="pt-15 pb-15 p-0 bg-deep-gray border border-light-gray mb-6 rounded-xl flex items-center flex-col">
+      <h1 className="text-[18px] text-white">{message}</h1>
+      <p className="mt-3 text-primary-gray text-center text-[14px] w-[80%]">
+        {messageTwo}
+      </p>
     </div>
   );
 }
