ab59b324edb88ac6ffb6b295ce41abd4f51500fc
olayode demola <demolaolayode6@gmail.com>
2026-08-05 15:10:49 +0100

    feat : import ReactCountryFlag component, and used it to display flags on each currency

--- src/features/RateChecker/SelectCurrency.jsx
+++ src/features/RateChecker/SelectCurrency.jsx
@@ -1,15 +1,17 @@
 import { useCurrencyList } from "../../context/CurrencyListContext";
+import ReactCountryFlag from "react-country-flag";
+import { currencyToCountry } from "../../data/currencies";
 
-function SelectCurrency({ styles, selectCurrency }) {
+function SelectCurrency({ styles, selectCurrency, show, setClickedCurr }) {
   const { currencies } = useCurrencyList();
-  // console.log(currencies);
+
   return (
     <div
       className={`bg-secondary-gray p-2 border border-[#585858] rounded-lg hidde absolute left-4  w-[85%] h-70 overflow-y-auto ${styles}`}
     >
       <input
         type="text"
-        className="p-2 pl-4 pr-4 border border-[#9D9D9D] mb-4 w-full rounded-lg text-primary-gray outline-0"
+        className="p-2 pl-4 pr-4 border border-primary-gray mb-4 w-full rounded-lg text-primary-gray outline-0"
         placeholder="Search currency..."
       />
       <div className="popular">
@@ -20,10 +22,29 @@ function SelectCurrency({ styles, selectCurrency }) {
         <ul>
           {currencies.map((currency) => (
             <li
-              onClick={() => selectCurrency(currency.iso_code)}
-              className="pr-1 pl-1 p-2 text-white"
+              onClick={() => {
+                selectCurrency(currency.iso_code);
+                show((displayed) => !displayed);
+                setClickedCurr(currency.iso_code);
+              }}
+              className="pr-1 pl-1 p-2 text-white flex items-center"
             >
+              <span className="mr-3">
+                <ReactCountryFlag
+                  countryCode={currencyToCountry[currency.iso_code]}
+                  svg
+                  style={{
+                    width: "24px",
+                    height: "24px",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                  }}
+                />
+              </span>
               {currency.iso_code}
+              <span className="ml-3 text-primary-gray">
+                <p className="text-[12px]">{currency.name}</p>
+              </span>
             </li>
           ))}
         </ul>
