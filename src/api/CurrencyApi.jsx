621eeb2ada34dd4df1433f485ed687cca0dc91b9
olayode demola <demolaolayode6@gmail.com>
2026-08-06 02:22:25 +0100

    feat: implment api error handling

--- src/api/CurrencyApi.jsx
+++ src/api/CurrencyApi.jsx
@@ -1,37 +1,76 @@
 const BASE_URL = "https://api.frankfurter.dev/v2";
-import { useTabContext } from "../context/TabContext";
 
-export async function getCurrencies(setterFtn, target) {
+export async function getCurrencies(setterFtn, target, setIsLoading, setError) {
+  setError(false);
   try {
+    setIsLoading(true);
     const res = await fetch(`${BASE_URL}/${target}`);
+
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+
     const data = await res.json();
     setterFtn(data);
-    console.log(data);
+    setIsLoading(false);
   } catch (error) {
+    setIsLoading(false);
+    setError(true);
     console.log(error);
   }
 }
 
-export async function getExchangeRate(target, base, quote, setterFtn) {
+export async function getExchangeRate(
+  target,
+  base,
+  quote,
+  setterFtn,
+  setIsLoading,
+  setError,
+) {
   try {
+    setIsLoading(true);
+
     const res = await fetch(`${BASE_URL}/${target}/${base}/${quote}`);
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+
     const data = await res.json();
     setterFtn(data);
-    console.log(data);
+    setIsLoading(false);
   } catch (error) {
+    setIsLoading(false);
+    setError(true);
     console.log(error);
   }
 }
 
-export async function getExchangeHistory(base, quote, date, setterFtn) {
+export async function getExchangeHistory(
+  base,
+  quote,
+  date,
+  setterFtn,
+  setIsLoading,
+  setError,
+) {
+  setError(false);
   try {
+    setIsLoading(true);
+
     const res = await fetch(
       `${BASE_URL}/rates?base=${base}&quotes=${quote}&from=${date}`,
     );
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
     const data = await res.json();
+
     setterFtn(data);
-    console.log(data);
+    setIsLoading(false);
   } catch (error) {
+    setIsLoading(false);
+    setError(true);
     console.log(error);
   }
 }
