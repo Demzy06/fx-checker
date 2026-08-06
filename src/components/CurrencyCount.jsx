78314b45ff0abdd9fdcfa4e316d617d11fffae96
olayode demola <demolaolayode6@gmail.com>
2026-08-06 02:22:59 +0100

    feat: update currencies number

--- src/components/CurrencyCount.jsx
+++ src/components/CurrencyCount.jsx
@@ -3,7 +3,7 @@ function CurrencyCount() {
     <div className="flex items-center">
       <ul className="flex justify-between text-primary-gray uppercase text-[11px] gap-x-6 list-d">
         <li>
-          <span className="mr-3">33</span>Currencies
+          <span className="mr-3">201</span>Currencies
         </li>
         <li className="list-disc">EDD</li>
         <li className="list-disc">ECB data</li>
